a61baebbdd8756631b8359300006bb56ec7bfcdc
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-22 01:24:18 -0300

    feat: add component Chat

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.css';
+import { useParams } from 'react-router-dom';
+
+function Chat() {
+  const { roomId } = useParams();
+  return (
+    <div className="chat">
+      <h1>You are in the {roomId} room</h1>
+    </div>
+  );
+}
+
+export default Chat;
