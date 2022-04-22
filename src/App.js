8c301810805c13ba299ebe8d7e11c46c77b1868e
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-22 01:25:54 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header';
 import SideBar from './components/Sidebar/SideBar';
+import Chat from './components/Chat/Chat';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Slack Clone</h1>
-      <Header />
-      <div className="app__body">
-        <SideBar />
-        {/* React Router */}
-      </div>
+      <BrowserRouter>
+        <Header />
+        <div className="app__body">
+          <SideBar />
+          <Routes>
+            <Route path="/room/:roomId" element={<Chat />} />
+            <Route path="/" element={<h3>Welcome</h3>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
