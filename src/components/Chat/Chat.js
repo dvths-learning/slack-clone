2ed38a256ebd733e3ac1f81dd2ec90ac6abd9896
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-22 18:17:27 -0300

    feat: chat header

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
+import StarBorderOutlineIcon from '@material-ui/icons/StarBorderOutlined';
+import InfoOutlineIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
   const { roomId } = useParams();
   return (
     <div className="chat">
-      <h1>You are in the {roomId} room</h1>
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">
+            <strong># general</strong>
+            <StarBorderOutlineIcon />
+          </h4>
+        </div>
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlineIcon /> Details
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
