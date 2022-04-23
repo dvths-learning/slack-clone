156c5207f78fc6f8e59c4633deab9783c94cf328
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-23 11:03:22 -0300

    feat: working on Google auth

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.js';
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
