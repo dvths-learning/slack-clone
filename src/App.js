744150d6c06459b7c99092704178e6cf57b3ef4c
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-23 11:03:44 -0300

    feat: add Login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import SideBar from './components/Sidebar/SideBar';
+import Login from './components/Login/Login';
 import Chat from './components/Chat/Chat';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <div className="app__body">
-          <SideBar />
-          <Routes>
-            <Route path="/room/:roomId" element={<Chat />} />
-            <Route path="/" element={<h3>Welcome</h3>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Router>
+        {!user ? (
+          <h1>
+            <Login />
+          </h1>
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <SideBar />
+              <Routes>
+                <Route path="/room/:roomId" element={<Chat />} />
+                <Route path="/" element={<h3>Welcome</h3>} />
+              </Routes>
+            </div>
+          </>
+        )}
+      </Router>
     </div>
   );
 }
