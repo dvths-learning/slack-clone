59696ae496bc091f78a6f425f9ac9776e1f372af
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-23 13:19:28 -0300

    feat: wrap the App with provider, initial state and reducer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StateProvider } from './context/StateProvider';
+import reducer, { initialState } from './helpers/reducer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
