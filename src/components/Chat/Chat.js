8bb7b1446debdd1566506a41375e80c7fa4901d2
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-22 22:49:43 -0300

    feat: messages renderization

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,11 +3,13 @@ import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlineIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlineIcon from '@material-ui/icons/InfoOutlined';
+import Message from '../Message/Message';
 import db from '../../firebase';
 
 function Chat() {
   const { roomId } = useParams();
   const [roomDetails, setRoomDetails] = useState(null);
+  const [roomMessages, setRoomMessages] = useState([]);
 
   useEffect(() => {
     if (roomId) {
@@ -15,6 +17,14 @@ function Chat() {
         .doc(roomId)
         .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
     }
+
+    db.collection('rooms')
+      .doc(roomId)
+      .collection('messages')
+      .orderBy('timeStamp', 'asc')
+      .onSnapshot((snapshot) =>
+        setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+      );
   }, [roomId]);
 
   return (
@@ -31,6 +41,17 @@ function Chat() {
             <InfoOutlineIcon /> Details
           </p>
         </div>
+      </div>
+      <div className="chat__messages">
+        {roomMessages.map(({ message, timestamp, user, userImage }) => (
+          <Message
+            // TODO: Definir unica key={}
+            message={message}
+            timestamp={timestamp}
+            user={user}
+            userImage={userImage}
+          />
+        ))}
       </div>
     </div>
   );
