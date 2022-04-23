08d4780fc1c941c62b90b22dae177aa761b1eab8
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-23 12:31:50 -0300

    feat: authentication done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,29 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
-import './Login.js';
+import './Login.css';
+import { auth, provider } from '../../firebase';
 
 function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div className="login">
       <div className="login__container">
-        <img src="" alt="" />
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg"
+          alt="slack-logo"
+        />
+        <h1>Sign in to Slack Clone</h1>
+        <p>slackclone.slack.com</p>
+        <Button onClick={signIn}>Sign in with Google</Button>
       </div>
     </div>
   );
