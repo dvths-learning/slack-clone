105ab934c9c88fc7e99c24bb85683d52ee450237
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-23 00:18:44 -0300

    fix: typo in "message" and correct timestamp renderization

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Message.css';
 
-function Message({ message, timestamp, user, userImage }) {
+function Message({ message, timeStamp, user, userImage }) {
   return (
-    <div className="messasge">
+    <div className="message">
       <img src={userImage} alt={user} />
       <div className="message__info">
-        <h4>{user} timestamp...</h4>
+        <h4>
+          {user}, {new Date(timeStamp?.toDate()).toUTCString()}
+        </h4>
         <p>{message}</p>
       </div>
     </div>
