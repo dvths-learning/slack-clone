0a597f91d3b2d53206a658a2abe6025f6d75b9f0
Tiago Henrique <tiagohs.dev@gmail.com>
2022-04-25 02:03:32 -0300

    feat: add information text

---
+++ src/components/Information/Information.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Information.css';
+
+function Information() {
+  return (
+    <div className="info">
+      <h3>Welcome!</h3>
+      <p>
+        This is a basic Slack clone that simulates your primary functionality:
+        add channels and send posts. <br />
+        Add channels and send messages in real time!
+      </p>
+    </div>
+  );
+}
+
+export default Information;
